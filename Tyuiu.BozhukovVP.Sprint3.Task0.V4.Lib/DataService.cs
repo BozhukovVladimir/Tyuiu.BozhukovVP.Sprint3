4ab1554f40bc3900ffb4951e9069180eb661027e
Vladimir Bozhukov <bozhukovvladimir@yandex.ru>
2024-10-30 16:19:31 +0500

    Добавьте файлы проекта.

---
+++ Tyuiu.BozhukovVP.Sprint3.Task0.V4.Lib/DataService.cs
@@ -0,0 +1,17 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint3;
+namespace Tyuiu.StoyakinNM.Sprint3.Task0.V4.Lib
+{
+    public class DataService : ISprint3Task0V4
+    {
+        public double GetMultiplySeries(int startValue, int stopValue)
+        {
+            double sumSeries = 1;
+            int i;
+            for (i = startValue; i <= stopValue; i++)
+            {
+                sumSeries = sumSeries * (Math.Sin(0.1) + i);
+            }
+            return Math.Round(sumSeries, 3);
+        }
+    }
+}
---
+++ Tyuiu.BozhukovVP.Sprint3.Task0.V4/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Tyuiu.BozhukovVP.Sprint3.Task0.V4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
