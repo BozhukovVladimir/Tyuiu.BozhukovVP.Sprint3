fd193a167645676490caaab5b0163fab8d0f668d
Vladimir Bozhukov <bozhukovvladimir@yandex.ru>
2024-10-30 17:32:55 +0500

    Task2

---
+++ Tyuiu.BozhukovVP.Sprint3.Task2.V10.Test/DataServiceTest.cs
@@ -0,0 +1,19 @@
+using Tyuiu.BozhukovVP.Sprint3.Task2.V10.Lib;
+namespace Tyuiu.BozhukovVP.Sprint3.Task2.V10.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidGetMultiplySeries()
+        {
+            DataService ds = new DataService();
+            int value = 1;
+            int startValue = 1;
+            int stopValue = 13;
+            double res = ds.GetMultiplySeries(value, startValue, stopValue);
+            double wait = 12288;
+            Assert.AreEqual(wait, res);
+        }
+    }
+}
---
+++ Tyuiu.BozhukovVP.Sprint3.Task2.V10/Program.cs
@@ -0,0 +1,44 @@
+﻿using Tyuiu.BozhukovVP.Sprint3.Task2.V10.Lib;
+namespace Tyuiu.BozhukovVP.Sprint3.Task2.V10
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Спринт #3 | Выполнил: Божуков В. П. | ИБКСб-24-1";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #3                                                               *");
+            Console.WriteLine("* Тема: Оператор цикла do-while                                           *");
+            Console.WriteLine("* Задание #2                                                              *");
+            Console.WriteLine("* Вариант #14                                                             *");
+            Console.WriteLine("* Выполнил: Божуков Владимир Павлович | ИБКСб-24-1                        *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Написать программу используя цикл do...while, которая вычисляет         *");
+            Console.WriteLine("* произведение ряда по формуле, при t = 1                                 *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            DataService ds = new DataService();
+            int value = 1;
+            int startValue = 1;
+            int stopValue = 13;
+
+            Console.WriteLine("Переменная t = " + value);
+            Console.WriteLine("Старт шага = " + startValue);
+            Console.WriteLine("Конец шага = " + stopValue);
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine("Произведение ряда = " + ds.GetMultiplySeries(value, startValue, stopValue));
+            Console.ReadKey();
+        }
+    }
+}
